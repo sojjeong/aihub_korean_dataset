1bb3993511011f92f2577dbd7bbfedc49563df4b
sojjeong <tip9004@naver.com>
2020-08-04 11:01:25 +0900

    Update text_crop.py
    Update text_crop.py
    Delete json_crop.py , checklist.txt

--- text_crop.py
+++ text_crop.py
@@ -3,25 +3,22 @@ import json
 import os
 import matplotlib.pyplot as plt
 import argparse
-
 from PIL import Image
-# from pprint import pprint as pp
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_json_dir', '-inJson', type=str, help='input json directory')
-parser.add_argument('--input_img_dir', '-inImg', type=str, help='input image directory, before mode')
-parser.add_argument('--output_dir', '-out', type=str, help='output image directory')
+parser.add_argument('--input_json_dir', '-inJson', type=str, default='data/textinthewild_data_info.json', help='input json directory')
+parser.add_argument('--input_img_dir', '-inImg', type=str, default='data/book', help='input image directory, before mode')
+parser.add_argument('--output_dir', '-out', type=str, default='data/crop_results', help='output image directory')
 parser.add_argument('--unit', '-u', type=int, default=0, help='select text unit option[0:character,1:word,2:both text]')
-parser.add_argument('--name', '-n', type=int, default=0, help='select naming option[0:id_gt, 1:gt_id]')
+parser.add_argument('--name', '-n', type=int, default=1, help='select naming option[0:id_textId_gt, 1:gt_id_textId]')
 
 opt = parser.parse_args()
-
 
 def json_open(json_dir, img_dir):
     """
     - json file open
     - image file name read
-    :param path: input json dir, input image dir
+    :param: input json dir, input image dir
     :return: image_data_list, label_data_list, image name list
     """
     with open(json_dir, 'rt', encoding='UTF8') as f:
@@ -30,7 +27,7 @@ def json_open(json_dir, img_dir):
         label_data_list = data['annotations']
 
     image_name_list = os.listdir(img_dir)
-
+    print("loading complete")
     return image_data_list, label_data_list, image_name_list
 
 
@@ -47,24 +44,26 @@ def finding_valid_image(image_data_list, image_name_list):
         file_name = image_data_list[i]['file_name']
         image_id = image_data_list[i]['id']
 
-        if image_data_list[i]['file_name'] in image_name_list:
-            # 다른 아이디로 중복된 파일 이름이 존재함
-            # 파일 이름을 key 로 사용해 중복 제거
+        if file_name in image_name_list:
+            # exists duplicated file name haven another id
+            # because dictionary does not allow duplicate keys,
+            # so, use the file name as key
             reverse_dict[file_name] = image_id
 
-    # 관리를 위해 id : file_name 으로 변경
+    # reverse {file_name:id} -> {id : file_name}
     for key, val in reverse_dict.items():
         image_dict[val] = key
 
+    print("finding valid image complete")
     return image_dict
 
 
-def finding_valid_label(label_data_list, image_dict):
+def finding_valid_label(label_data_list, image_dict, input_unit_option):
     """
     finding label which has same image id in image_dict
     :param label_data_list:[{'id': , 'image_id': , 'attribues':{'class': character, word }, 'text': , 'bbox':[x,y coordinate]}]
-    :param image_dict: {image_id:file_name}
-    :return: labels_dict{'image id':[{id: , 'text':[bbox coordinate]}, {id: , 'text':[bbox coordinate]}....]}
+    :param image_dict: {'image_id' : 'file_name'}
+    :return: labels_dict{'image id':[{id:{'text':[bbox coordinate]}}, {id:{'text':[bbox coordinate]}}...]}
     """
     valid_dict = {}
 
@@ -84,112 +83,113 @@ def finding_valid_label(label_data_list, image_dict):
         elif dict['attributes']['class'] == 'word':
             unit_option = 1
 
-        if opt.unit == 2:
+        if input_unit_option == 2:
             unit_option = 2
 
-        if opt.unit == unit_option:
+        if input_unit_option == unit_option:
             if image_id in image_dict:
                 temp_dict = {}
                 text = dict['text']
 
                 # except for not characters
-                is_not_ko = False
-                for character in '.,./\?<>~!@#$%^&*()_+-=;:` "''':
-                    if text == character:
-                        is_not_ko = True
-
-                # except for invalid length label and not character
-                if is_not_ko or len(text) > 1:
+                if text is None:
                     continue
 
-                temp_dict['id'] = dict['id']
-                temp_dict[text] = dict['bbox']
+                # except for invalid length
+                if unit_option == 0:
+                    if len(text) > 1:
+                        continue
+
+                is_not_ch = False
+
+                # check valid ascii value
+                for ch in text:
+                    ascii_ch = ord(ch)
+                    if not((ascii_ch >= 44032 and ascii_ch <= 55203) or     # Korean
+                           (ascii_ch >= 48 and ascii_ch <= 57) or           # Number
+                            (ascii_ch >= 65 and ascii_ch <= 90) or          # English(capital)
+                            (ascii_ch >= 97 and ascii_ch <= 122)):          # English(small)
+                        is_not_ch = True
+
+                if is_not_ch:
+                    continue
+
+                text_dict = {}
+                text_dict[text] = dict['bbox']
+
+                text_id = dict['id']
+                temp_dict[text_id] = text_dict
 
                 valid_dict[image_id].append(temp_dict)
 
+    print("finding valid label complete")
     return valid_dict
 
 
-def finding_path(image_dict, input_path):
+def bbox_crop(valid_image_dict, valid_label_dict, intput_folder, output_folder):
     """
-    modify the image dictionary for extracting value
-    :param image_dict:
-    :param input_path: input image path
-    :return: image path dictionary {'image_id':image path}
-    """
-    path_dict = {}
-
-    for image_id, path_list in image_dict.items():
-        image_type = path_list[0]
-
-        # match type name to input,output image folder name
-        if image_type == 'product':
-            image_type = 'Goods'
-        elif image_type == 'sign':
-            image_type = 'Signboard'
-        elif image_type == 'traffic sign':
-            image_type = 'Traffic_Sign'
-
-        file_name = path_list[1]
-
-        file_path = os.path.join(input_path, image_type, file_name)
-        is_path = os.path.exists(file_path)
-
-        # input the image file path in dictionary
-        # {'image_id':image path}
-        if is_path:
-            path_dict[image_id] = file_path
-        else:
-            continue
-
-    # pp(path_dict)
-    return path_dict
-
-
-def images_crop(path_dict, label_dict, output_path):
-    """
-    :param path_dict: image path dictionary {'image id':image path}
-    :param label_dict: label list {'image_id':{'text':bbox list}
-                            bbox = [left, top, w, h]
-    :param output_path : output path for storing cropped image
+    valid text box crop in the valid image
+    :param valid_image_dict:
+    :param valid_label_dict:
     :return:
     """
-    for image_id, path in path_dict.items():
+
+    # make output folder
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    for image_id, file_name in valid_image_dict.items():
+        path = f'{intput_folder}/{file_name}'
+
+        # check the dir
         is_file_exists = os.path.isfile(path)
-        if is_file_exists is False:
+        if not is_file_exists:
             continue
 
         load_image = Image.open(path)
-        bbox_dict = label_dict.get(image_id)
+        text_list = valid_label_dict.get(image_id)  # [{'00001' :  {'text':[bbox]}}, {'00001' :  {'text':[bbox]}}]
 
-        for bbox_text, bbox_coordinate in bbox_dict.items():
-            text = bbox_text
-            coordinate = []
+        for text_instance in text_list:
+            print(text_instance)
+            for id, box in text_instance.items():
+                text_id = id
+                text_bbox = box
 
-            # modify negative coordinates
-            for i in bbox_coordinate:
-                if i < 0:
-                    i *= -1
-                elif i == 0:    # invalid h,w size changed to greater than 0
-                    i = 1
-                coordinate.append(i)
+            for text, bbox in text_bbox.items():
+                # invalid h,w size changed to greater than 0
+                coordinate = []
 
-            left = coordinate[0]
-            top = coordinate[1]
-            w = coordinate[2]
-            h = coordinate[3]
-            right = left + w
-            bottom = top + h
+                for i in bbox:
+                    if i < 0:
+                        i *= -1
+                    elif i == 0:
+                        i = 1
+                    coordinate.append(i)
 
-            crop_image = load_image.crop((left, top, right, bottom))
+                left = coordinate[0]
+                top = coordinate[1]
+                w = coordinate[2]
+                h = coordinate[3]
+                right = left + w
+                bottom = top + h
 
-            # print(image_id, text)
-            if opt.name == 0:
-                write_path = "{}/{}_{}.jpg".format(output_path, image_id, text)
-            elif opt.name == 1:
-                write_path = "{}/{}_{}.jpg".format(output_path, text, image_id)
+                origin_x, origin_y = load_image.size
 
-            crop_image.save(write_path)
+                # pass invalid w, h size
+                if (w > origin_x or h > origin_y or
+                    right > origin_x or bottom > origin_y):
+                    continue
+
+                crop_image = load_image.crop((left, top, right, bottom))
+
+                # naming option
+                if opt.name == 0:
+                    write_path = "{}/{}_{}_{}.jpg".format(output_folder, image_id, text_id, text)
+                elif opt.name == 1:
+                    write_path = "{}/{}_{}_{}.jpg".format(output_folder, text, image_id, text_id)
+
+                crop_image.save(write_path)
+    print("cropping complete")
 
 
 def imshow(image):
@@ -201,9 +201,10 @@ def main(opt):
     image_data_list, label_data_list, image_name_list = json_open(json_dir=opt.input_json_dir, img_dir=opt.input_img_dir)
 
     valid_image_dict = finding_valid_image(image_data_list, image_name_list)
-    valid_labels_dict = finding_valid_label(label_data_list, valid_image_dict)
+    valid_labels_dict = finding_valid_label(label_data_list, valid_image_dict, opt.unit)
+
+    bbox_crop(valid_image_dict, valid_labels_dict, opt.input_img_dir, opt.output_dir)
 
 
 if __name__ == "__main__":
     main(opt)
-    # branch test
